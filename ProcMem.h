2590f46e13b420b7057d941ac1d7bb1cb514cea6
Weeke <baileydunlap9@gmail.com>
2023-05-01 19:03:40 -0600

    Update ProcMem.h

--- ProcMem.h
+++ ProcMem.h
@@ -1,1 +1,46 @@
+#ifndef PROCMEM_H
+#define PROCMEM_H
 
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+class ProcMem {
+public:
+    ProcMem();
+    ~ProcMem();
+
+    bool Process(const wchar_t* processName);
+    DWORD Module(const wchar_t* moduleName);
+    DWORD GetProcessID() const;
+
+    template <typename T>
+    T Read(DWORD address) {
+        T value;
+        SIZE_T bytesRead;
+        if (!ReadProcessMemory(hProcess, reinterpret_cast<LPCVOID>(address), &value, sizeof(T), &bytesRead) || bytesRead != sizeof(T)) {
+            throw std::runtime_error("Failed to read memory.");
+        }
+        return value;
+    }
+
+    template <typename T>
+    bool Write(DWORD address, T value) {
+        SIZE_T bytesWritten;
+        if (!WriteProcessMemory(hProcess, reinterpret_cast<LPVOID>(address), &value, sizeof(T), &bytesWritten) || bytesWritten != sizeof(T)) {
+            throw std::runtime_error("Failed to write memory.");
+            return false;
+        }
+        return true;
+    }
+
+private:
+    DWORD processID;
+    HANDLE hProcess;
+
+    DWORD GetModuleBaseAddress(DWORD processID, const wchar_t* moduleName);
+};
+
+#endif // PROCMEM_H
