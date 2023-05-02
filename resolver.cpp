37b28652a43a9c4e53b2a8a6786f40099e22fe03
Weeke <baileydunlap9@gmail.com>
2023-05-01 19:35:04 -0600

    Create resolver.cpp

---
+++ resolver.cpp
@@ -0,0 +1,110 @@
+#include "ProcMem.h"
+#include "csgo.hpp"
+#include "offsets.hpp"
+#include <cmath>
+#include <vector>
+
+float NormalizeYaw(float yaw) {
+    while (yaw < -180.0f) yaw += 360.0f;
+    while (yaw > 180.0f) yaw -= 360.0f;
+    return yaw;
+}
+
+// Custom data structure to store enemy behavior data
+struct EnemyData {
+    float velocity;
+    float animationState;
+    bool wasHit;
+};
+
+// Store enemy data in a buffer
+std::vector<EnemyData> enemyDataBuffer;
+
+void BruteResolver(ProcMem& mem, DWORD clientBase, DWORD localPlayerBase, DWORD entityBase) {
+    // Read the enemy's current yaw
+    float currentYaw = mem.Read<float>(entityBase + hazedumper::netvars::m_angEyeAnglesY);
+    
+    // This is a simple example of a resolver that tries to "bruteforce" the correct yaw
+    // You'll need to develop a more sophisticated algorithm to accurately predict the hitbox positions
+    for (int i = 0; i < 4; ++i) {
+        float testYaw = currentYaw + 45.0f * i;
+        float normalizedTestYaw = NormalizeYaw(testYaw);
+        
+        // Write the testYaw back to the enemy's m_angEyeAnglesY
+        mem.Write<float>(entityBase + hazedumper::netvars::m_angEyeAnglesY, normalizedTestYaw);
+        
+        // Test if the new yaw yields better results (e.g., by checking if the shots hit the enemy)
+        // If successful, you can break the loop and use this yaw
+    }
+}
+
+void CollectEnemyData(ProcMem& mem, DWORD entityBase) {
+    EnemyData enemyData;
+
+    // Collect enemy velocity
+    float velX = mem.Read<float>(entityBase + hazedumper::netvars::m_vecVelocity + 0x0);
+    float velY = mem.Read<float>(entityBase + hazedumper::netvars::m_vecVelocity + 0x4);
+    float velZ = mem.Read<float>(entityBase + hazedumper::netvars::m_vecVelocity + 0x8);
+    enemyData.velocity = std::sqrt(velX * velX + velY * velY + velZ * velZ);
+
+    // Collect enemy animation state (you'll need to find the correct offset)
+    enemyData.animationState = mem.Read<float>(entityBase + /* ANIMATION_STATE_OFFSET */ 0);
+
+    // Collect hit status (you'll need a method to determine if the enemy was hit)
+    enemyData.wasHit = /* CHECK_IF_ENEMY_WAS_HIT_METHOD */ false;
+
+    // Store the collected data in the buffer
+    enemyDataBuffer.push_back(enemyData);
+
+    // Optionally, limit the buffer size to a certain length
+    if (enemyDataBuffer.size() > 100) {
+        enemyDataBuffer.erase(enemyDataBuffer.begin());
+    }
+}
+
+void DataDrivenResolver(ProcMem& mem, DWORD entityBase) {
+    // Read the enemy's current yaw
+    float currentYaw = mem.Read<float>(entityBase + hazedumper::netvars::m_angEyeAnglesY);
+
+    // Initialize variables for the best yaw and the highest hit rate
+    float bestYaw = currentYaw;
+    float highestHitRate = 0.0f;
+
+    // Increase the resolution of the tested yaws
+    const int numYawSteps = 8;
+    for (int i = 0; i < numYawSteps; ++i) {
+        float testYaw = currentYaw + (360.0f / numYawSteps) * i;
+        float normalizedTestYaw = NormalizeYaw(testYaw);
+
+        // Calculate the hit rate for the current test yaw using the collected data
+        float hitCount = 0;
+        float totalCount = 0;
+        int buffer_size = static_cast<int>(enemyDataBuffer.size());
+        for (int i = 0; i < buffer_size; ++i) {
+            const auto& enemyData = enemyDataBuffer[i];
+
+            // Introduce a weighting system
+            float weight = static_cast<float>(i + 1) / buffer_size;
+
+            // Check if the test yaw would hit the enemy based on the enemy data
+            if (enemyData.animationState > 0.5f &&
+                std::abs(normalizedTestYaw - currentYaw) < 45.0f &&
+                /* Consider other factors such as distance, weapon type, player stance */) {
+
+                hitCount += weight;
+            }
+            totalCount += weight;
+        }
+
+        float hitRate = hitCount / totalCount;
+
+        // If the hit rate is higher than the previous best, update the best yaw and highest hit rate
+        if (hitRate > highestHitRate) {
+            highestHitRate = hitRate;
+            bestYaw = normalizedTestYaw;
+        }
+    }
+
+    // Write the best yaw back to the enemy's m_angEyeAnglesY
+    mem.Write<float>(entityBase + hazedumper::netvars::m_angEyeAnglesY, bestYaw);
+}
