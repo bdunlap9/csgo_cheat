1ce9e2546c774cf97cd2dcd879c43c50d31b0ec9
Weeke <baileydunlap9@gmail.com>
2023-05-03 21:42:18 -0600

    Update resolver.cpp

--- resolver.cpp
+++ resolver.cpp
@@ -103,10 +103,6 @@ float GetDistance(ProcMem& mem, DWORD localPlayerBase, DWORD entityBase) {
     return distance;
 }
 
-struct GameState {
-    // Add your game state variables here
-};
-
 std::vector<std::pair<double, GameState>> gameStates;
 
 double get_current_time() {
@@ -411,6 +407,16 @@ float CalculateWeightBasedOnVelocityAndAimAndMovement(float enemyVelX, float ene
     return weight;
 }
 
+float AdjustHitRateBasedOnAnimationState(float hitRate, float animationState, float positiveThreshold, float negativeThreshold, float positiveFactor, float negativeFactor) {
+    // Adjust hit rate based on enemy's animation state using the provided thresholds and factors
+    if (animationState > positiveThreshold) {
+        hitRate *= positiveFactor;
+    } else if (animationState < negativeThreshold) {
+        hitRate *= negativeFactor;
+    }
+    return hitRate;
+}
+
 void DataDrivenResolver(ProcMem& mem, DWORD localPlayerBase, DWORD entityBase) {
     try {
         // Read the enemy's current yaw
@@ -418,17 +424,6 @@ void DataDrivenResolver(ProcMem& mem, DWORD localPlayerBase, DWORD entityBase) {
     } catch (const std::runtime_error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
-
-    // Set the number of yaw steps based on the average velocity and yaw change rate
-    int numYawSteps = 16; // Increase the default number of steps
-
-    // Read the enemy's LBY value
-    float enemyLBY = mem.Read<float>(entityBase + offsets::netvars::m_flLowerBodyYawTarget);
-    float lbyDelta = std::abs(enemyLBY - lastLBY);
-    lastLBY = enemyLBY;
-
-    // Check if the enemy is breaking LBY
-    bool isBreakingLBY = lbyDelta > 35.0f;
 
     // Initialize variables for the best yaw and the highest hit rate
     float bestYaw = currentYaw;
@@ -450,23 +445,17 @@ void DataDrivenResolver(ProcMem& mem, DWORD localPlayerBase, DWORD entityBase) {
     avgYawChangeRate /= (buffer_size - 1);
 
     // Set the number of yaw steps based on the average velocity and yaw change rate
-    int numYawSteps = 8; // Default value
-    if (avgVelocity > 250.0f || avgYawChangeRate > 30.0f) {
-        numYawSteps = 16;
-    }
+    int numYawSteps = GetNumYawSteps(avgVelocity, avgYawChangeRate);
 
     for (int i = 0; i < numYawSteps; ++i) {
         float testYaw = currentYaw + (360.0f / numYawSteps) * i;
         float normalizedTestYaw = NormalizeYaw(testYaw);
 
         // Read the enemy's velocity
-        float enemyVelX = mem.Read<float>(entityBase + offsets::netvars::m_vecVelocity + 0x0);
-        float enemyVelY = mem.Read<float>(entityBase + offsets::netvars::m_vecVelocity + 0x4);
-        float enemyVelZ = mem.Read<float>(entityBase + offsets::netvars::m_vecVelocity + 0x8);
+        Vector3 enemyVelocity = GetEnemyVelocity(mem, entityBase);
 
-                // Read local player's aim angles
-        float localPlayerAimPitch = mem.Read<float>(localPlayerBase + offsets::netvars::m_angEyeAnglesX);
-        float localPlayerAimYaw = mem.Read<float>(localPlayerBase + offsets::netvars::m_angEyeAnglesY);
+        // Read local player's aim angles
+        Vector2 localPlayerAimAngles = GetLocalPlayerAimAngles(mem, localPlayerBase);
 
         float hitCount = 0;
         float totalCount = 0;
@@ -475,44 +464,32 @@ void DataDrivenResolver(ProcMem& mem, DWORD localPlayerBase, DWORD entityBase) {
             const auto& enemyData = enemyDataBuffers[entityBase][i];
 
             // Introduce a weighting system
-            float weight = static_cast<float>(i + 1) / buffer_size;
+            float weight = GetWeightBasedOnIndex(i, buffer_size);
 
             // Check if the test yaw would hit the enemy based on the enemy data and additional factors
             if (enemyData.animationState > 0.5f && std::abs(normalizedTestYaw - currentYaw) < 45.0f) {
                 float distance = GetDistance(mem, localPlayerBase, entityBase);
 
                 // Adjust the weight based on distance
-                if (distance > 200.0f && distance < 300.0f) {
-                    weight *= 1.5f;
-                } else if (distance > 300.0f) {
-                    weight *= 2.0f;
-                }
+                weight = AdjustWeightBasedOnDistance(distance, weight);
 
                 // Adjust the weight based on enemy's velocity and local player's aim angles
-                weight *= CalculateWeightBasedOnVelocityAndAimAndMovement(enemyVelX, enemyVelY, enemyVelZ, localPlayerAimPitch, localPlayerAimYaw);
+                weight = AdjustWeightBasedOnVelocityAndAim(enemyVelocity, localPlayerAimAngles, weight);
 
                 if (enemyData.wasHit) {
                     hitCount += weight;
                 }
                 totalCount += weight;
-                
+
                 // Adjust the weight based on LBY breaking
-                if (isBreakingLBY) {
-                    weight *= 1.5f;
-                }
+                weight = AdjustWeightForLBYBreaking(enemyData.isBreakingLBY, weight);
             }
         }
 
         float hitRate = hitCount / totalCount;
 
         // Use the enemy's animation state to further adjust the hit rate
-        if (enemyAnimationState > 0.5f) {
-            hitRate *= 1.2f;
-        }
-        else {
-            // enemyAnimationState < -0.5f
-            hitRate *= 0.8f;
-        }
+        hitRate = AdjustHitRateBasedOnAnimationState(hitRate, enemyDataBuffers[entityBase].back().animationState);
 
         // If the hit rate is higher than the previous best, update the best yaw and highest hit rate
         if (hitRate > highestHitRate) {
@@ -527,7 +504,7 @@ void DataDrivenResolver(ProcMem& mem, DWORD localPlayerBase, DWORD entityBase) {
     float normalizedSmoothedYaw = NormalizeYaw(smoothedYaw);
 
     try {
-        mem.Write<float>(entityBase + offsets::netvars::m_angEyeAnglesY, bestYaw);
+        mem.Write<float>(entityBase + offsets::netvars::m_angEyeAnglesY, normalizedSmoothedYaw);
     } catch (const std::runtime_error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }  
